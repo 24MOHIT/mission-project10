b23d83f707684e12d55813f43b08f06fa415058f
Mohit Prajapat <mohitprajapat242004@gmail.com>
2025-09-18 12:35:22 +0530

    18-09-2025

---
+++ ORSProject-10-UI/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HttpServiceService } from './http-service.service';
+import { EndpointServiceService } from './endpoint-service.service';
+import { ServiceLocatorService } from './service-locator.service';
+import { AuthServiceService } from './auth-service.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+
+  ],
+  imports: [
+    HttpClientModule,
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    HttpServiceService,
+    EndpointServiceService,
+    ServiceLocatorService,
+    AuthServiceService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
